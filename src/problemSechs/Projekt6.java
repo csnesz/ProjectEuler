f73bb34dc8cabf70c61ae6ab31136fd637c40153
Linda <62564937+csnesz@users.noreply.github.com>
2021-02-07 00:04:50 +0100

    Beschreibung hinzufügt

--- src/problemSechs/Projekt6.java
+++ src/problemSechs/Projekt6.java
@@ -2,6 +2,22 @@ package problemSechs;
 
 public class Projekt6 {
 
+	
+	/*
+	  Sum square difference
+	Show HTML problem content 
+	Problem 6
+	The sum of the squares of the first ten natural numbers is,
+
+	The square of the sum of the first ten natural numbers is,	
+
+	Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+
+	Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+
+
+	 */
 	public static void main(String[] args) {
 		long result=squaresSum(10)-sumSqaures(10);
 		System.out.println(result);
